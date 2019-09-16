7748012bef8ffd79e576d68dd873f78b7b871d72
chriskpeterson <chriskpeterson@gmail.com>
2019-09-15 20:21:54 -0500

    Create config.js

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+title: 'Chris Portfolio',
+description: 'just playing around',
+themeConfig: {
+    nav: [
+        { text: 'Home', link: '/'},
+        { text: 'About', link: '/guide'},
+        { text: 'Resume', link: '/resume'},
+        { text: 'Illustrations', link: 'illustrations.html'},
+        { text: 'Writing samples', link: 'writingsamples.html'}
+    ],
+    
+    sidebar: 'auto'
+}
+}
