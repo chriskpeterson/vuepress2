6c42686509f3a7fec07faa2712cefb869cc60b35
chriskpeterson <chriskpeterson@gmail.com>
2019-09-18 20:35:45 -0500

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -4,11 +4,8 @@ description: 'Information Developer Portfolio',
 themeConfig: {
     nav: [
         { text: 'Home', link: '/'},
-        { text: 'About', link: '/about'},
         { text: 'Resume', link: '/resume'},
-        { text: 'Rewrites', link: 'rewrites.html'},
         { text: 'Writing samples', link: 'writingsamples.html'}
-        { text: 'Innovation', link: 'innovation.html'}
     ],
     
     sidebar: 'auto'
