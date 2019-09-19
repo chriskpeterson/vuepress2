ebdaf5962688fa02a4a0aeeab7b2ab712c15d1a0
Chris Peterson <chriskpeterson@gmail.com>
2019-09-18 21:10:08 -0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,11 @@
 module.exports = {
 title: 'Chris Peterson',
-description: 'Information Developer Portfolio',
+description: 'Information Developer portfolio',
 themeConfig: {
     nav: [
         { text: 'Home', link: '/'},
         { text: 'About', link: '/about'},
         { text: 'Resume', link: '/resume'},
-        { text: 'Rewrites', link: 'rewrites.html'},
         { text: 'Writing samples', link: 'writingsamples.html'}
     ],
     
